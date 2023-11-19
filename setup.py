d32fa3469e1847b074349d19b5cf2304ef2de891
Kira1108 <327485253@163.com>
2023-11-19 19:53:46 +0800

    Initial commit

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 from setuptools import find_packages
 
+requires = ["transformers","datasets","accelerate","evaluate",'googletrans',
+ "tiktoken","langchain","chromadb","torch","tensorflow","sentencepiece","nltk","openai==1.3.3"]
+
 setup(name='nlp_toolkit',
       version='0.0.1',
       description='use nlp toolkit to power your nlp projects',
       author='The fastest man alive.',
       packages=find_packages(),
-      install_requires=[])
+      install_requires=requires)
+
+
+
 
 
 
