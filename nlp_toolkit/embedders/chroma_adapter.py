153a25918ef9c85e5ec0fbd8a38e55a5eff0bf42
Kira1108 <327485253@163.com>
2023-12-07 00:34:13 +0800

    Initial commit

--- nlp_toolkit/embedders/chroma_adapter.py
+++ nlp_toolkit/embedders/chroma_adapter.py
@@ -1,10 +1,15 @@
 from .base import BaseEmbedder
+import numpy as np
 
 def make_chroma(embedder:BaseEmbedder):
     from chromadb import Documents, EmbeddingFunction, Embeddings
 
     class ChromaEmbeddingFunction(EmbeddingFunction):
         def __call__(self, input: Documents) -> Embeddings:
-            return embedder(input)
+            result = embedder(input)
+            if isinstance(result, list):
+                return result
+            else:
+                return result.tolist()
         
     return ChromaEmbeddingFunction()
